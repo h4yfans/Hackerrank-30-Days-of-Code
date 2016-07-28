91c8672e2bdd9179666f052d2c28bf8050ab5511
Kaan Karaca <kaan94karaca@gmail.com>
2016-07-29 02:42:46 +0300

    Day 11 2D Arrays

---
+++ src/Day112DArrays.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Kaan on 28.07.2016.
+ */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day112DArrays {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        int sum = -999;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int currentSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (currentSum > sum) {
+                    sum = currentSum;
+                }
+            }
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
